0d4153dda7df60798c5b15a9c25c4dbe0b66b54f
Kennet <you@example.com>
2019-01-09 16:32:02 +0100

    ok

---
+++ app/src/main/java/com/example/kenne/countries/QuizActivity.java
@@ -0,0 +1,20 @@
+package com.example.kenne.countries;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+
+    public void goToCurrentScoreActivity(View view){
+        Intent intent = new Intent(this, CurrentScoreActivity.class);
+        startActivity(intent);
+    }
+}
