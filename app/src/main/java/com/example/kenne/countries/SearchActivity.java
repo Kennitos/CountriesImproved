b26a0f9a83f48e7ad727d5556292d7765bc7524e
Kennet <you@example.com>
2019-01-10 14:21:09 +0100

    API works now

--- app/src/main/java/com/example/kenne/countries/SearchActivity.java
+++ app/src/main/java/com/example/kenne/countries/SearchActivity.java
@@ -3,14 +3,31 @@ package com.example.kenne.countries;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private static ArrayList<String> allNames;
+//    private static String[] COUNTRIES = new String[]{};
+//    private static String[] COUNTRIES
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Intent intent = getIntent();
+        ArrayList<Country> countriesArray = (ArrayList) intent.getStringArrayListExtra("countries");
+        Log.d("connect_test","all countries:"+countriesArray.get(0).getName());
+
+        for(int i = 0; i < countriesArray.size(); i++){
+            allNames.add(countriesArray.get(i).getName());
+        }
     }
 
     public void goToCountryDetail(View view){
